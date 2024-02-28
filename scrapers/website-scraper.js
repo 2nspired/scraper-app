fdc78a59fe42db53e85d9e8dbf952f51f4426493
Thomas Trudzinski <thomastrudzinski@gmail.com>
2024-02-28 09:28:18 -1000

    using API to pull a list of bills, then using list of bills to pull specific bill data and add to the original bill object.

--- scrapers/website-scraper.js
+++ scrapers/website-scraper.js
@@ -5,8 +5,10 @@ import * as cheerio from 'cheerio';
 const port = 4000;
 const app = Express();
 
-// INSERT URL HERE
+// INSERT URL TO CAPTURE FROM HERE
 const url = 'https://hnldoc.ehawaii.gov/hnldoc/browse/bills';
+
+// LOADS PAGE VIA  PUPPETEER, TAKES SCREENSHOT, AND SCRAPES DATA
 
 (async () => {
     const browser = await puppeteer.launch();
@@ -46,7 +48,5 @@ console.log(bills);
 await browser.close();
 })().catch(err => console.log(err))
 
-
+// ROUTES
 app.listen(4000, () => {console.log(`Server is running on port ${port}`)});
-
-
